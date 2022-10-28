3a37ba0232e038fbd8b24df96c2a14e2b16b5590
Buhancev <maxim.buhancev@gmail.com>
2022-10-29 01:13:43 +0700

    Annotation @Quialifier

--- src/main/java/com/bbuhha/Dog.java
+++ src/main/java/com/bbuhha/Dog.java
@@ -1,5 +1,8 @@
 package com.bbuhha;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Dog implements Pet {
     public Dog() {
         System.out.println("Dog bean created");
--- src/main/java/com/bbuhha/Person.java
+++ src/main/java/com/bbuhha/Person.java
@@ -1,6 +1,7 @@
 package com.bbuhha;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("personBean")
@@ -13,6 +14,9 @@ public class Person {
     //он позволяет реализовывать неизменяемые компоненты и гарантировать что требуемые
     //зависимости не null
 
+    //допустим подходят Cat и Dog, то Spring очевидно не может сам решить неоднозначность
+    //предотварить выброс данного исключения можно с помощью аннотации @Quilifier
+    //она пишется после @Autowired
     private Pet pet;
     private String surname;
     private int age;
@@ -48,18 +52,19 @@ public class Person {
     @Autowired и пытается ввести зависимость. Каким образом? - Он ищет подходящие bean'ы
     по типу Pet. Он находит Cat, т.к класс Cat implement Pet. Успешно внедряет зависимость.
     Если ни один bean не будет соответстовать типа Pet, то не комплируется (ген. искл).
-    Если найдется больше одного bean'a для типа Pet, то ... ?
+    Если найдется больше одного bean'a для типа Pet, то @Qualifier
 
     @Autowired //если в классе имеется 1 конструктор, то можно не писать аннотацию
     //начиная с какой-то там версии Spring сам дописывает это, но это скам
-    public Person(Pet pet) {
+    public Person(@Qualifier("catBean") Pet pet) {
         System.out.println("Person bean created");
         this.pet = pet;
     }
     */
 
     //конверация из файла xml: set -> setPet
-    //@Autowired //в принципе вот и всё, при использовании аннотации имя сеттера не важно
+    @Autowired //в принципе вот и всё, при использовании аннотации имя сеттера не важно
+    //Qualifier("catBean")
     public void setPet(Pet pet) {
         System.out.println("Class Person: anyMethodName Pet");
         this.pet = pet;
