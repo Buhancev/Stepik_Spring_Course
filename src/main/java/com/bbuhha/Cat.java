bf0a4b671315f149ac18e38484e2a2be742ca2c9
Buhancev <maxim.buhancev@gmail.com>
2022-10-29 00:27:04 +0700

    config with annotation

--- src/main/java/com/bbuhha/Cat.java
+++ src/main/java/com/bbuhha/Cat.java
@@ -1,5 +1,13 @@
 package com.bbuhha;
 
+import org.springframework.stereotype.Component;
+
+//в скобках указываем id bean'a
+//можно id не указывать, тогда default bean id = имя класса, но с прописной первой буквой
+//в данном случае id = id = cat
+//FavoriteSong -> id = favoriteSong
+//SQLtext -> id = SQLtext (если 2 заглавные в начале идут подряд, то не меняется)
+@Component
 public class Cat implements Pet{
     public Cat() {
         System.out.println("Cat bean created");
