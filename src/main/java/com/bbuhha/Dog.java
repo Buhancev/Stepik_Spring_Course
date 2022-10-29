1913cdee6626fd241d2c34f9c70da03854fb067e
Buhancev <maxim.buhancev@gmail.com>
2022-10-29 10:42:55 +0700

    @Scope, @PostConstruct, @PostDestroy

--- src/main/java/com/bbuhha/Dog.java
+++ src/main/java/com/bbuhha/Dog.java
@@ -1,8 +1,13 @@
 package com.bbuhha;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
+@Scope
 public class Dog implements Pet {
     public Dog() {
         System.out.println("Dog bean created");
@@ -13,9 +18,11 @@ public class Dog implements Pet {
         System.out.println("Bow-Wow");
     }
 
+    @PostConstruct
     protected void init() {
         System.out.println("Class Dog: init method");
     }
+    @PreDestroy
     private void destroy() {
         System.out.println("Class Dog: destroy method");
     }
