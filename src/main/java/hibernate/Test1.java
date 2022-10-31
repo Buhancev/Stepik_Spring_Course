f17cc3f3bbd1c1dcbde90bcdce2f578968175ff0
Buhancev <maxim.buhancev@gmail.com>
2022-11-01 01:17:05 +0700

    @GeneratedValue - PrimaryKey

--- src/main/java/hibernate/Test1.java
+++ src/main/java/hibernate/Test1.java
@@ -1,2 +1,40 @@
-package hibernate;public class Test1 {
+package hibernate;
+
+import hibernate.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class Test1 {
+    public static void main(String[] args) {
+        //должны создать сессию, чтобы иметь доступ к БД
+        //для начала нужно создать SessionFactory
+        //достаточно создать 1 раз, можно переиспользовать
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")//SessionFactory узнает как создать сессию
+                .addAnnotatedClass(Employee.class) //Entity класс
+                .buildSessionFactory(); //постройка
+
+        try {
+            //Обертка вокруг подключения к базе с помощью JDBC, если не будет сессии то не сможем CRUD
+            Session session = factory.getCurrentSession();
+            //получаем сессию, делаем операцию и всё (недолго живет, в отличие от SessionFactory)
+
+            Employee emp = new Employee("Sveta", "Belaya",
+                    "Yandex", 1000000);
+
+            //открывается транзакция
+            session.beginTransaction();
+            session.save(emp); //insert
+            session.getTransaction().commit(); //подтверждаем свои действия
+        }
+
+        finally {
+            factory.close(); //factory может выбрасывать исключение, а нам всегда нужно
+            //его закрывать, поэтому можно сделать так(нужно)
+        }
+
+
+    }
 }
