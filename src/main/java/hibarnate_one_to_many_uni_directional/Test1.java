6733070803a4aa6531176819c9d365a40c1c4981
Buhancev <maxim.buhancev@gmail.com>
2022-11-03 23:49:35 +0700

    One-to-Many & Many-to-One, FOREIGN KEY from TARGET table, UNI-directional

---
+++ src/main/java/hibarnate_one_to_many_uni_directional/Test1.java
@@ -0,0 +1,63 @@
+package hibarnate_one_to_many_uni_directional;
+
+import hibarnate_one_to_many_uni_directional.entity.Department;
+import hibarnate_one_to_many_uni_directional.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class Test1 {
+    public static void main(String[] args) {
+        //должны создать сессию, чтобы иметь доступ к БД
+        //для начала нужно создать SessionFactory
+        //достаточно создать 1 раз, можно переиспользовать
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")//SessionFactory узнает как создать сессию
+                .addAnnotatedClass(Employee.class) //Entity класс
+                .addAnnotatedClass(Department.class)
+                .buildSessionFactory(); //постройка
+
+        Session session = null;
+        try {
+            //Обертка вокруг подключения к базе с помощью JDBC, если не будет сессии то не сможем CRUD
+             session = factory.getCurrentSession();
+            //получаем сессию, делаем операцию и всё (недолго живет, в отличие от SessionFactory)
+
+            //Department department = new Department("HR", 500, 1500);
+            //Employee emp1 = new Employee("Sbeta", "Belaya", 800);
+            //Employee emp2 = new Employee("Max", "Ruf", 900);
+
+            //department.addEmployeeToDepartment(emp1);
+            //department.addEmployeeToDepartment(emp2);
+
+            //открывается транзакция
+            session.beginTransaction();
+            //********************************
+
+            //session.save(department);
+            //Department department = session.get(Department.class, 4);
+            //System.out.println(department);
+            //System.out.println(department.getEmps());
+
+
+            //Employee employee = session.get(Employee.class, 6);
+            //у работника нет обратной связи, поэтому департамент не удалится как в примере с Bi directional
+            //session.delete(employee);
+
+            //Department department = session.get(Department.class, 4);
+            //session.delete(department);
+
+            session.getTransaction().commit(); //подтверждаем свои действия
+
+        }
+
+        finally {
+            session.close(); //если будет ошибка, то закроем сессию
+            factory.close(); //factory может выбрасывать исключение, а нам всегда нужно
+            //его закрывать, поэтому можно сделать так(нужно)
+        }
+
+
+    }
+}
---
+++ src/main/java/hibarnate_one_to_many_uni_directional/entity/Employee.java
@@ -0,0 +1,93 @@
+package hibarnate_one_to_many_uni_directional.entity;
+
+/*JPA - Java Persistence API - стандартная спецификация, которая описывает систему
+для управления сохранением Java-object в таблицы БД
+
+Hibernate - самая популярная РЕАЛИЗАЦИЯ спецификации JPA
+
+jpa - описывает правила, hibernate - реализует эти правила
+Поэтому используем аннотации JPA (даже команда хайбернейт это рекомендует)
+Hibernate - реализует JPA.
+ */
+import jakarta.persistence.*;
+
+@Entity //класс который имеет отображение в БД в виде таблицы
+@Table(name="employees") //к какой таблице привязываем класс, если бы названия были одинаковы
+//то можно было бы не писать
+public class Employee {
+    @Id //данное поле является отображение PrimaryKey в таблице
+    //Описывает стратегию для генерации столбца PrimaryKey
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    /*
+    IDENTITY - автоматическое увеличение стобца по правила, описанных в БД
+    SEQUENCE - полагается на работу Sequence в БД (в MySQL такого нет)
+    TABLE - устаревший способ, полагается на уникальность (наибольшее значение +1)
+    то есть сохраняет прошлое значение (скам) полагает на столбец в БД
+    AUTO - default GenerationType. Выбор стратегии будет зависеть от БД (лучше самому прописывать)
+     */
+
+    @Column(name="id") //к какому именно столбцу привязываем
+    private int id;
+
+    @Column(name="name")
+    private String name;
+    @Column(name="surname")
+    private String surname;
+
+    @Column(name="salary")
+    private int salary;
+
+
+
+    public Employee() {
+    }
+
+    public Employee(String name, String surname, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
