b5b91e2c7f8756198ba31849a4142ca40e264b9a
Buhancev <maxim.buhancev@gmail.com>
2022-11-13 18:14:16 +0700

    GET: employee by id, @PathVarable

--- src/main/java/com/bbuhha/spring/rest/controller/MyRestController.java
+++ src/main/java/com/bbuhha/spring/rest/controller/MyRestController.java
@@ -4,6 +4,7 @@ import com.bbuhha.spring.rest.entity.Employee;
 import com.bbuhha.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,14 @@ public class MyRestController {
     @GetMapping("employees")
     public List<Employee> showAllEmployees() {
         List<Employee> allEmployees = employeeService.getAllEmployees();
-        return allEmployees;
+        return allEmployees; //отправляет List<Employee>, который Spring преобразует в json
     }
 
-    //@GetMapping("/employees")
+    @GetMapping("/employees/{id}") //Получаем id из тела метода GET http-request
+    //аннотация PathVariable используется для получения значения переменной из АДРЕСА запроса
+    public Employee showEmployeeById(@PathVariable int id) {
+
+        Employee employee = employeeService.getEmployeeById(id);
+        return employee; //отправляет Employee, который Spring преобразует в json
+    }
 }
