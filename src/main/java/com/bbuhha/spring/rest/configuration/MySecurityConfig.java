c0f4fc51c8e318ab5e1d156fa3431ec677f4a2c8
Buhancev <maxim.buhancev@gmail.com>
2022-11-15 18:22:47 +0700

    users, password, roles in db, passwords non-crypt

--- src/main/java/com/bbuhha/spring/rest/configuration/MySecurityConfig.java
+++ src/main/java/com/bbuhha/spring/rest/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.bbuhha.spring.rest.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -7,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
+
+import javax.sql.DataSource;
 
 
 @EnableWebSecurity //помечаем класс, как класс ответственный за Security конфигурацию
@@ -26,25 +29,30 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
             // other public endpoints of your API may be appended to this array
     };
 
+    @Autowired
+    DataSource dataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder(); //дефолт шифрование
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder(); //дефолт шифрование
+//
+//        //hardcoded
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder
+//                        .username("Maxim")
+//                        .password("Maxim")
+//                        .roles("EMPLOYEE"))
+//                .withUser(userBuilder
+//                        .username("Sveta")
+//                        .password("Sveta")
+//                        .roles("HR"))
+//                .withUser(userBuilder
+//                        .username("Danila")
+//                        .password("Danila")
+//                        .roles("MANAGER", "HR"));
 
-        //hardcoded
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder
-                        .username("Maxim")
-                        .password("Maxim")
-                        .roles("EMPLOYEE"))
-                .withUser(userBuilder
-                        .username("Sveta")
-                        .password("Sveta")
-                        .roles("HR"))
-                .withUser(userBuilder
-                        .username("Danila")
-                        .password("Danila")
-                        .roles("MANAGER", "HR"));
+        //инфа о юзерах в бд
+        auth.jdbcAuthentication().dataSource(dataSource);
 
     }
 
